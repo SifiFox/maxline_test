cfd655bbf35052b456d7fb7e9b9b2664f2df966d
SifiFox <dima.antonovich.1999@gmail.com>
2025-09-25 16:31:56 +0300

    fix: hero position

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
       <Header />
       <Bonus />
       <div className="flex flex-col justify-end relative xl:flex-row flex-1">
-        <Hero className="w-full absolute inset-0 -top-20 md:top-auto md:bottom-0 xl:bottom-0 xl:top-auto -left-4 md:-left-80 xl:left-10 -z-10" />
-        <FormLayout className="h-fit xl:h-full rounded-t-3xl lg:rounded-t-none relative xl:mt-0 xl:fixed xl:bottom-0 xl:right-0" />
+        <Hero className="w-full absolute inset-0 bottom-40 top-auto md:bottom-0 xl:bottom-0 xl:top-auto -left-4 md:-left-80 xl:left-10 -z-10" />
+        <FormLayout className="h-fit xl:h-full rounded-t-3xl lg:rounded-t-none relative xl:fixed xl:bottom-0 xl:right-0" />
       </div>
     </Layout>
   );
